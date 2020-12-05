1f41fd649a52df4f603a26cf3ee250dc812bd107
Teh Zhen Rong <zhenrong1999@student.usm.my>
2020-12-06 02:37:55 +0800

    animation done

--- gasket4.js
+++ gasket4.js
@@ -4,11 +4,32 @@ var program;
 var points = [];
 var colors = [];
 var NumTimesToSubdivide = 3;
-var speed = 100;
+var speed = 5;
+var paused = false;
+var programInfo;
+default_state = {
+	transx: 0,
+	transy: 0,
+	rotation: 0,
+	scaling: 1,
+	speed: 50,
+	pause_state: false,
+};
+current_state = {
+	transx: 0,
+	transy: 0,
+	rotation: 0,
+	scaling: 0.5,
+	speed: 50,
+	pause_state: false,
+};
 
+var current_rotation;
 window.onload = function init() {
 	canvas = document.getElementById("gl-canvas");
-
+	document.getElementById("slide").value = current_state.speed;
+	change_speed(document.getElementById("slide"));
+	current_rotation = document.getElementById("current_rotation");
 	gl = WebGLUtils.setupWebGL(canvas);
 	if (!gl) {
 		alert("WebGL isn't available");
@@ -56,50 +77,161 @@ window.onload = function init() {
 		vColor: gl.getAttribLocation(program, "vColor"),
 		umodelViewMatrix: gl.getUniformLocation(program, "uModelViewMatrix"),
 	};
-
-	requestAnimationFrame(render_rotate_180(programInfo));
+	render_engine(programInfo);
 };
+function change_state(id) {
+	if (paused == true) {
+		id.innerHTML = "Stop";
+		paused = false;
+		render_engine(programInfo);
+		//requestAnimationFrame(render_using_delay);
+	} else {
+		id.innerHTML = "Play";
+		paused = true;
+	}
+}
 
-function render_rotate_180(programInfo) {
-	var then = 0;
-	var rotation = 0;
-	var rotate_reached = false;
-	speed = 100;
-	function render_rotate(now) {
-		now *= 0.001; // convert to seconds
-		deltaTime = (now - then) * speed;
-		then = now;
-		transx = 0;
-		transy = 0;
-		scaling = 0.5;
+function change_speed(id) {
+	current_state.speed = id.value;
+	document.getElementById("current_speed").innerHTML = current_state.speed;
+}
 
-		if (rotate_reached != true) {
-			if (rotation + deltaTime < 180) rotation += deltaTime;
-			else {
-				rotation = 180;
-				rotate_reached = true;
-			}
-		} else {
-			if (rotation - deltaTime > 0) rotation -= deltaTime;
-			else {
-				rotation = 0;
-				rotate_reached = false;
-			}
+var then = 0;
+var animation = 7;
+var animation_rotate_left = true;
+var target_rotation = 180;
+var done = false;
+var point_goto = [0, 0];
+function render_engine(programInfo) {
+	function rotate_animation_calc(deltaTime, target_rotation) {
+		remainder = target_rotation - current_state.rotation;
+		if (Math.abs(remainder) > deltaTime)
+			current_state.rotation += Math.sign(remainder) * deltaTime;
+		else {
+			current_state.rotation = target_rotation;
+			done = true;
 		}
-
-		modelViewMatrix = modelViewMatrixCalculation(
-			transx,
-			transy,
-			rotation,
-			scaling
-		);
-		drawScene(programInfo, modelViewMatrix);
-
-		if (rotation != 0 || rotate_reached != true)
-			requestAnimationFrame(render_rotate);
 	}
 
-	requestAnimationFrame(render_rotate);
+	function scaling_animation(deltaTime, target_scale) {
+		deltascaletime = deltaTime / 250;
+		remainder = target_scale - current_state.scaling + deltascaletime;
+		if (Math.abs(remainder) > deltascaletime)
+			current_state.scaling += Math.sign(remainder) * deltascaletime;
+		else {
+			current_state.scaling = target_scale;
+			done = true;
+		}
+	}
+	function moving_animtation(deltaTime, target_position) {
+		deltascaletime = deltaTime / 100;
+		remainderx = target_position[0] - current_state.transx;
+		remaindery = target_position[1] - current_state.transy;
+		remainder = Math.pow(
+			Math.pow(remainderx, 2) + Math.pow(remaindery, 2),
+			0.5
+		);
+		if ((Math.abs(remainderx) + Math.abs(remaindery))/2 < deltascaletime) {
+			current_state.transx = target_position[0];
+			current_state.transy = target_position[1];
+			done = true;
+		}
+		if (Math.abs(remainderx) > deltascaletime/2) {
+			current_state.transx +=
+				(remainderx) * deltascaletime /remainder ;
+		}
+		if (Math.abs(remaindery) > deltascaletime/2) {
+			current_state.transy +=
+				(remaindery) * deltascaletime /remainder ;
+		}
+	}
+
+	function random_point_for_moving() {
+		//top-right: 0.6,0.53
+		//top-left: -0.6,0.53
+		//bottom-left: -0.6,-0.76
+		//bottom-right: 0.6,-0.76
+		position = Math.floor(Math.random() * 100) % 4;
+		var point = [0, 0];
+		random_y = (Math.random() * 10) / 9;
+		if (position < 2) {
+			point[1] = 1.29 * Math.sin((Math.random() * Math.PI) / 2) - 0.76;
+			point[1]=Math.round(point[1]*1000)/1000;
+			if (position == 0) {
+				point[0] = 0.6;
+			} else {
+				point[0] = -0.6;
+			}
+		} else {
+			point[0] = 1.2 * Math.sin((Math.random() * Math.PI) / 2) - 0.6;
+			point[0] = Math.round(point[0]*1000)/1000;
+			if (position == 2) {
+				point[1] = -0.76;
+			} else {
+				point[1] = 0.53;
+			}
+		}
+		return point;
+	}
+	function render_frame(now) {
+		if (paused == false) {
+			now *= 0.001;
+			var deltaTime = (now - then) * current_state.speed;
+			then = now;
+			current_rotation.innerHTML = 1;
+			modelViewMatrix = modelViewMatrixCalculation(
+				current_state.transx,
+				current_state.transy,
+				current_state.rotation,
+				current_state.scaling
+			);
+			if (current_state.pause_state == false) {
+				if (animation == 1) {
+					rotate_animation_calc(deltaTime, 180);
+				} else if (animation == 2) {
+					rotate_animation_calc(deltaTime, 0);
+				} else if (animation == 3) {
+					rotate_animation_calc(deltaTime, -180);
+				} else if (animation == 4) {
+					rotate_animation_calc(deltaTime, 0);
+				} else if (animation == 5) {
+					scaling_animation(deltaTime, 0.9);
+				} else if (animation == 6) {
+					scaling_animation(deltaTime, 0.5);
+				} else if (animation == 7) {
+					moving_animtation(deltaTime, point_goto);
+				}
+			} else current_state.pause_state = false;
+
+			modelViewMatrix = modelViewMatrixCalculation(
+				current_state.transx,
+				current_state.transy,
+				current_state.rotation,
+				current_state.scaling
+			);
+			current_rotation.innerHTML = deltaTime;
+			document.getElementById("current_rotation").innerHTML = animation;
+			drawScene(programInfo, modelViewMatrix);
+
+			if (done != true) {
+				requestAnimationFrame(render_frame);
+				//current_state.iterations++;
+			} else {
+				if (++animation <= 6) {
+					done = false;
+					requestAnimationFrame(render_frame);
+				} else {
+					animation = 7;
+					point_goto = random_point_for_moving();
+					done = false;
+					requestAnimationFrame(render_frame);
+				}
+			}
+		} else {
+			current_state.pause_state = true;
+		}
+	}
+	requestAnimationFrame(render_frame);
 }
 
 function modelViewMatrixCalculation(transx, transy, rotation, scaling) {
@@ -221,3 +353,42 @@ function render(programInfo) {
 	);
 	gl.drawArrays(gl.TRIANGLES, 0, points.length);
 }
+
+/*
+function render_using_delay() {
+	var transx = current_state.transx;
+	var transy = current_state.transy;
+	var rotation = current_state.rotation;
+	var scaling = current_state.scaling;
+
+	if (paused == false) {
+		modelViewMatrix = modelViewMatrixCalculation(
+			current_state.transx,
+			current_state.transy,
+			current_state.rotation,
+			current_state.scaling
+		);
+		//
+
+		//current_state.rotation++;
+		if (current_state.rotate_to_left == false) {
+			if (current_state.rotation < 180) current_state.rotation++;
+			else {
+				current_state.rotation--;
+				current_state.rotate_to_left = true;
+			}
+		} else {
+			if (current_state.rotation > 0) current_state.rotation--;
+			else {
+				current_state.rotation = 0;
+				current_state.rotate_to_left = false;
+			}
+		}
+		drawScene(programInfo, modelViewMatrix);
+		var t = setTimeout(
+			requestAnimationFrame(render_using_delay),
+			current_state.delay
+		);
+		
+	}
+}*/
